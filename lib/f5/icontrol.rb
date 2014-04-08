174d381ae42d240c9a6e9679fb380c74ce91be38
Sean Walberg <sean@ertw.com>
2014-04-08 15:38:09 -0500

    Extract code to parent class

--- lib/f5/icontrol.rb
+++ lib/f5/icontrol.rb
@@ -10,5 +10,28 @@ module F5
       @username = username
       @password = password
     end
+
+    private
+
+    def wsdl_path
+      File.dirname(__FILE__).gsub /f5-icontrol\/lib\/.*/, "f5-icontrol/lib/wsdl/"
+    end
+
+
+
+    def client(api_group)
+      api_namespace = api_group.gsub /\./, '/'
+      endpoint = '/iControl/iControlPortal.cgi'
+      Savon.client(wsdl: "#{wsdl_path}#{api_group}.wsdl",
+                   endpoint: "https://#{@hostname}#{endpoint}",
+                   ssl_verify_mode: :none,
+                   basic_auth: [@username, @password],
+                   #log: true,
+                   #logger: Logger.new(STDOUT),
+                   #pretty_print_xml: true,
+                   #log_level: :debug,
+                   namespace: "urn:iControl:#{api_namespace}"
+                  )
+    end
   end
 end
--- lib/f5/icontrol/system/system_info.rb
+++ lib/f5/icontrol/system/system_info.rb
@@ -5,25 +5,11 @@ module F5
       class SystemInfo < F5::Icontrol
 
         def get_version
-          client.call(:get_version).xpath('//m:get_versionResponse').inner_text.gsub /\n/, ""
+          client("System.SystemInfo").call(:get_version).xpath('//m:get_versionResponse').inner_text.gsub /\n/, ""
         end
 
         private
 
-        def client
-          wsdl_path = File.dirname(__FILE__) + "/../../../wsdl/System.SystemInfo.wsdl"
-          @endpoint = '/iControl/iControlPortal.cgi'
-          Savon.client(wsdl: wsdl_path,
-                       endpoint: "https://#{@hostname}#{@endpoint}",
-                       ssl_verify_mode: :none,
-                       basic_auth: [@username, @password],
-                       #log: true,
-                       #logger: Logger.new(STDOUT),
-                       #pretty_print_xml: true,
-                       #log_level: :debug,
-                       namespace: "urn:iControl:System/SystemInfo"
-                      )
-        end
       end
     end
   end
