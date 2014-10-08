45f5d6c9c0734b19f4d63e8f31e962163b92610f
Sean Walberg <sean@ertw.com>
2014-10-08 13:04:23 -0500

    Fixing pathname fixing for WSDL
    Didn't take into account that the installed version would have a version

--- lib/f5/icontrol.rb
+++ lib/f5/icontrol.rb
@@ -17,16 +17,14 @@ module F5
     private
 
     def wsdl_path
-      File.dirname(__FILE__).gsub /f5-icontrol\/lib\/.*/, "f5-icontrol/lib/wsdl/"
+      File.dirname(__FILE__).gsub /(f5-icontrol[^\/]*\/lib)\/.*/, "\\1/wsdl/"
     end
-
-
 
     def client(api_group)
       api_namespace = api_group.gsub /\./, '/'
       endpoint = '/iControl/iControlPortal.cgi'
       @client_cache[api_group] ||=
-        Savon.client(wsdl: "#{wsdl_path}/#{api_group}.wsdl",
+        Savon.client(wsdl: "#{wsdl_path}#{api_group}.wsdl",
                      endpoint: "https://#{@hostname}#{endpoint}",
                      ssl_verify_mode: :none,
                      basic_auth: [@username, @password],
