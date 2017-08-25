40889fa82dc9d921ec8131df78b2c765da62e99b
David Alpert <david@spinthemoose.com>
2017-08-25 07:56:28 -0500

    add two LocalLB status enumerations (#10)
    * add EnabledStatus
    
    as per: https://devcentral.f5.com/wiki/iControl.LocalLB__EnabledStatus.ashx
    
    * add Availability_Status
    
    from: https://devcentral.f5.com/wiki/iControl.LocalLB__AvailabilityStatus.ashx

---
+++ lib/f5/icontrol/locallb/availability_status.rb
@@ -0,0 +1,31 @@
+module F5
+  module Icontrol
+    module LocalLB
+      # https://devcentral.f5.com/wiki/iControl.LocalLB__AvailabilityStatus.ashx
+      # A list of possible values for an object's availability status.
+      module AvailabilityStatus
+        # Error scenario.
+        AVAILABILITY_STATUS_NONE	= 0
+
+        # The object is available in some capacity.
+        AVAILABILITY_STATUS_GREEN	= 1
+
+        # The object is not available at the current
+        # moment, but may become available again even
+        # without user intervention.
+        AVAILABILITY_STATUS_YELLOW	= 2
+
+        # The object is not available, and will require
+        # user intervention to make this object available
+        # again.
+        AVAILABILITY_STATUS_RED	= 3
+
+        # The object's availability status is unknown.
+        AVAILABILITY_STATUS_BLUE	= 4
+
+        # The object's is unlicensed.
+        AVAILABILITY_STATUS_GRAY	= 5
+      end
+    end
+  end
+end
